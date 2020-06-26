48692424725d901f4c4e3d11b71dcbb48286b63a
7wf <7ywolf@gmail.com>
2020-06-25 22:19:20 -0300

    feat: setup warp http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+const PORT: u16 = 7331;
+
+#[tokio::main]
+async fn main() {
+    let index = warp::get().map(|| "https://github.com/statikksh");
+
+    println!("The server is listening at port {}.", PORT);
+    warp::serve(index).run(([127, 0, 0, 1], PORT)).await;
 }
