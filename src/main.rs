960243baefc990e4be979b288e20fbf366b04153
7wf <7ywolf@gmail.com>
2020-06-30 13:42:11 -0300

    feat: route error handler

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use warp::Filter;
+use std::convert::{Infallible};
+
+use warp::{Filter, Rejection, Reply};
+use warp::http::StatusCode;
 
 const PORT: u16 = 7331;
 
+async fn handle_rejection(error: Rejection) -> Result<impl Reply, Infallible> {
+    let (code, message) = if error.is_not_found() {
+        (StatusCode::NOT_FOUND, "Resource not found.")
+    } else {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error.")
+    };
+
+    Ok(warp::reply::with_status(warp::reply::html(message), code))
+}
+
 #[tokio::main]
 async fn main() {
-    let index = warp::get().map(|| "https://github.com/statikksh");
+    let index = warp::path::end().map(|| "https://github.com/statikksh/statikk");
+    let routes = warp::get().and(index).recover(handle_rejection);
 
     println!("The server is listening at port {}.", PORT);
-    warp::serve(index).run(([127, 0, 0, 1], PORT)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], PORT)).await;
 }
